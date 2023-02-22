c8d64ec1e1cae693dc0425c17b05642337827c73
Ángel Quiroz <angel.quiroz@edgebound.com>
2023-02-21 22:42:55 -0500

    feat: create `useSearchBar` hook

---
+++ src/hooks/useSearchBar.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useCallback } from "react";
+
+export default function useSearchBar() {
+  const [value, setValue] = useState("");
+  const [records, setRecords] = useState([]);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (typeof window === "undefined")
+      return;
+    const resultsFromLocalStorage = localStorage.getItem("results");
+    if (!resultsFromLocalStorage)
+      return;
+    const results = JSON.parse(resultsFromLocalStorage);
+    if (!Array.isArray(results))
+      return;
+    setRecords(results);
+  }, []);
+
+  const searchOnChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const searchOnKeyDown = (event) => {
+    if (event.key === "Enter") {
+      search();
+    }
+  };
+
+  const search = useCallback(() => {
+    if (!value)
+      setResults([]);
+    const results = records.filter((record) => record.toLowerCase().includes(value.toLowerCase())
+    );
+    setResults(results);
+  }, [value, records]);
+
+  return { value, results, searchOnChange, searchOnKeyDown, search };
+}
