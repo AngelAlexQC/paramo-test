916d1ff5caaaafc3501e3104b0134b0573187adf
Ángel Quiroz <angel.quiroz@edgebound.com>
2023-02-24 11:33:34 -0500

    fix: remove console error because video

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -20,11 +20,10 @@ export default function HomePage() {
             height="315"
             src="https://www.youtube.com/embed/Y1qQZbTF8iQ"
             title="YouTube video player"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
             className={styles.video}
-          ></iframe>
+          />
         </div>
         <div className={styles.feedRight}>
           <header className={styles.feedRightHeader}>
