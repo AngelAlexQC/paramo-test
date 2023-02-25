ced29c0fc688dfa8bcacf398bfdc3a18315ced14
Ángel Quiroz <angel.quiroz@edgebound.com>
2023-02-25 17:06:03 -0500

    feat: create "Back" link in Post page

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,6 +1,7 @@
 import styles from "./Post.module.css";
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 Post.propTypes = {
   post: PropTypes.shape({
     title: PropTypes.string.isRequired,
@@ -10,10 +11,17 @@ Post.propTypes = {
 
 function Post({ post = { title: "Post not found", content: "" } }) {
   return (
-    <article className={styles.post}>
-      <h1>{post.title}</h1>
-      <p dangerouslySetInnerHTML={{ __html: post.content }} />
-    </article>
+    <>
+      <nav>
+        <Link to="/blog">
+          <FaArrowLeft /> Back to blog
+        </Link>
+      </nav>
+      <article className={styles.post}>
+        <h1>{post.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: post.content }} />
+      </article>
+    </>
   );
 }
 
